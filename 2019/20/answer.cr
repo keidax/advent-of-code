9f1c541352d22aecc98cba600be8c91b50195e9d
Firstname Lastname <user@domain.com>
2019-12-22 01:32:21 -0500

    2019, Day 20

---
+++ 2019/20/answer.cr
@@ -0,0 +1,92 @@
+require "../expect"
+
+require "./grid"
+
+expect "Example 1", <<-EX, 23
+         A           
+         A           
+  #######.#########  
+  #######.........#  
+  #######.#######.#  
+  #######.#######.#  
+  #######.#######.#  
+  #####  B    ###.#  
+BC...##  C    ###.#  
+  ##.##       ###.#  
+  ##...DE  F  ###.#  
+  #####    G  ###.#  
+  #########.#####.#  
+DE..#######...###.#  
+  #.#########.###.#  
+FG..#########.....#  
+  ###########.#####  
+             Z       
+             Z       
+EX
+
+expect "Example 2", <<-EX, 58
+                   A               
+                   A               
+  #################.#############  
+  #.#...#...................#.#.#  
+  #.#.#.###.###.###.#########.#.#  
+  #.#.#.......#...#.....#.#.#...#  
+  #.#########.###.#####.#.#.###.#  
+  #.............#.#.....#.......#  
+  ###.###########.###.#####.#.#.#  
+  #.....#        A   C    #.#.#.#  
+  #######        S   P    #####.#  
+  #.#...#                 #......VT
+  #.#.#.#                 #.#####  
+  #...#.#               YN....#.#  
+  #.###.#                 #####.#  
+DI....#.#                 #.....#  
+  #####.#                 #.###.#  
+ZZ......#               QG....#..AS
+  ###.###                 #######  
+JO..#.#.#                 #.....#  
+  #.#.#.#                 ###.#.#  
+  #...#..DI             BU....#..LF
+  #####.#                 #.#####  
+YN......#               VT..#....QG
+  #.###.#                 #.###.#  
+  #.#...#                 #.....#  
+  ###.###    J L     J    #.#.###  
+  #.....#    O F     P    #.#...#  
+  #.###.#####.#.#####.#####.###.#  
+  #...#.#.#...#.....#.....#.#...#  
+  #.#####.###.###.#.#.#########.#  
+  #...#.#.....#...#.#.#.#.....#.#  
+  #.###.#####.###.###.#.#.#######  
+  #.#.........#...#.............#  
+  #########.###.###.#############  
+           B   J   C               
+           U   P   P               
+EX
+
+puts answer File.read("input.txt")
+
+def answer(input) : Int32
+  grid = Grid.new(input)
+
+  start_tile = grid.start_portal
+  end_tile = grid.end_portal
+
+  unvisited = [start_tile, end_tile]
+  start_tile.distance = 0
+
+  while unvisited.any?
+    unvisited.uniq!.sort_by! &.distance
+    next_tile = unvisited.shift
+
+    adjacent_distance = next_tile.distance + 1
+    unvisited_neighbors = next_tile.neighbors.reject! &.visited?
+
+    unvisited_neighbors.each { |t| t.distance = Math.min(t.distance, adjacent_distance) }
+    unvisited.concat(unvisited_neighbors)
+
+    next_tile.visited = true
+  end
+
+  end_tile.distance
+end
