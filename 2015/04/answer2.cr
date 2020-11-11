ce4abc26a46c80f9cb038dbc795900802f55e089
Gabriel Holodak <gthepiper@gmail.com>
2020-11-11 01:21:14 -0500

    2015, day 4

---
+++ 2015/04/answer2.cr
@@ -0,0 +1,12 @@
+require "./md5"
+
+secret = "bgvyzdsv"
+start = 0
+loop do
+  start += 1
+  md5 = MD5Reader.new(IO::Memory.new("#{secret}#{start}"))
+  break if md5.to_s.starts_with?("000000")
+  print '.' if start % 1000 == 0
+end
+puts
+puts start
