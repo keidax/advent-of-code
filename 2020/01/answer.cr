93b13c8aece7ba42449d800f0bbcd4fc5a56537b
Gabriel Holodak <gthepiper@gmail.com>
2020-12-01 00:15:57 -0500

    2020, day 1

---
+++ 2020/01/answer.cr
@@ -0,0 +1,20 @@
+nums = [] of Int32
+File.each_line("input.txt") do |line|
+  nums << line.to_i
+end
+
+# Part 1
+nums.combinations(size = 2).each do |combo|
+  if combo.sum == 2020
+    puts combo.product
+    break
+  end
+end
+
+# Part 2
+nums.combinations(size = 3).each do |combo|
+  if combo.sum == 2020
+    puts combo.product
+    break
+  end
+end
