1128710f88fe4e0078d911833e18c9037a453b7b
Gabriel Holodak <gthepiper@gmail.com>
2020-12-15 20:53:00 -0500

    add some optimizations for day 15

--- 2020/15/answer.cr
+++ 2020/15/answer.cr
@@ -1,7 +1,8 @@
 INPUT = [11, 18, 0, 20, 1, 7, 16]
 
 def find_number_spoken(turn)
-  last_spoken = Hash(Int32, Int32).new(initial_capacity: 4_000_000)
+  last_spoken = Array(Int32).new(size: turn, value: -1)
+
   INPUT.each_with_index do |num, i|
     last_spoken[num] = i
   end
@@ -10,13 +11,13 @@ def find_number_spoken(turn)
   last = INPUT.last
 
   while i < turn
-    prev_turn = last_spoken[last]?
+    prev_turn = last_spoken.unsafe_fetch(last)
 
     # Delay updating the age of the most recently spoken number
     # until we've retrieved the previous age
     last_spoken[last] = i - 1
 
-    if prev_turn
+    if prev_turn >= 0
       last = (i - prev_turn - 1)
     else
       last = 0
