0bedb1ae836316e33b999f5dd6b1e23198a97c93
Gabriel Holodak <gthepiper@gmail.com>
2022-12-01 23:49:40 -0500

    add a crystal helper library for 2022

--- 2022/01/answer.cr
+++ 2022/01/answer.cr
@@ -1,5 +1,10 @@
+require "../aoc"
+
+AOC.day!(1)
+
 nums = [] of Int32?
-File.each_line("input.txt") do |line|
+
+AOC.each_line do |line|
   if line.blank?
     nums << nil
   else
@@ -14,8 +19,5 @@ total_calories = nums
   }.map { |_, cals| cals.compact.sum }
   .sort
 
-# Part 1
-puts total_calories[-1]
-
-# Part 2
-puts total_calories[-3..-1].sum
+AOC.part1 { total_calories[-1] }
+AOC.part2 { total_calories[-3..-1].sum }
---
+++ 2022/aoc.cr
@@ -0,0 +1,70 @@
+require "http/request"
+require "http/client"
+
+module AOC
+  INPUT_PATH = "input.txt"
+
+  @@day : Int32 = 0
+
+  def self.day!(day : Int32)
+    @@day = day
+  end
+
+  @@input_loaded = false
+  @@input : String = ""
+
+  def self.each_line
+    load_input!
+
+    @@input.each_line do |line|
+      yield line
+    end
+  end
+
+  def self.input
+    load_input!
+
+    @@input
+  end
+
+  def self.part1
+    log("Part 1: #{yield}")
+  end
+
+  def self.part2
+    log("Part 2: #{yield}")
+  end
+
+  private def self.load_input!
+    unless @@input_loaded
+      @@input = load_input(@@day)
+    end
+  end
+
+  private def self.load_input(day)
+    input_path =
+      if File.exists?(INPUT_PATH)
+        File.read(INPUT_PATH)
+      else
+        download_file(day)
+      end
+  end
+
+  private def self.download_file(day)
+    log ">> downloading input for the first time"
+
+    client = HTTP::Client.new("adventofcode.com", tls: true)
+    get_input = HTTP::Request.new(method: "GET", resource: "/2022/day/#{day}/input")
+    get_input.cookies["session"] = ENV["AOC_SESSION"]
+
+    response = client.exec get_input
+    input = response.body
+
+    File.write(INPUT_PATH, input)
+    input
+  end
+
+  private def self.log(message)
+    puts "🎄 AOC Day #{@@day} #{message}"
+  end
+end
