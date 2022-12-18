b0088d34435425fe4b3ac6a3a16b8680eae9ec35
Gabriel Holodak <gthepiper@gmail.com>
2022-12-18 14:58:41 -0500

    2022, day 16, part 2

--- 2022/16/answer.cr
+++ 2022/16/answer.cr
@@ -72,29 +72,88 @@ map = valve_keys.to_h do |valve_name|
   {valve_name, distances_from_valve}
 end
 
-def max_release(time_left, current_release, current_node, map, remaining_nodes : Set(String)) : Int32
-  return current_release if remaining_nodes.empty?
+struct Actor
+  property moving_to : String
+  property distance : Int32
 
-  remaining_nodes.map do |next_node|
-    distance = map[current_node][next_node]
+  def initialize(@moving_to, @distance)
+  end
+end
 
-    new_time_left = time_left - distance - 1
-    if new_time_left <= 0
-      next current_release
+STATE_CACHE = {} of {Int32, Set(String)} => Int32
+
+def max_release_at_minute(minutes_left, actors : Array(Actor), current_release, map, remaining_nodes : Set(String)) : Int32
+  if minutes_left == 0
+    return current_release
+  end
+
+  actors = actors.map! { |actor| actor.distance -= 1; actor }
+  minutes_left -= 1
+
+  ready_actors, moving_actors = actors.partition { |actor| actor.distance == -1 }
+
+  ready_actors.each do |actor|
+    current_release += minutes_left * VALVES[actor.moving_to].flow_rate
+  end
+
+  if ready_actors.empty?
+    return max_release_at_minute(minutes_left, moving_actors, current_release, map, remaining_nodes)
+  end
+
+  if remaining_nodes.empty?
+    if moving_actors.empty?
+      # all nodes are visited
+      return current_release
+    else
+      return max_release_at_minute(minutes_left, moving_actors, current_release, map, remaining_nodes)
     end
+  end
 
-    new_release = current_release + new_time_left * VALVES[next_node].flow_rate
+  if remaining_nodes.size < ready_actors.size
+    # there are fewer nodes than actors left, so we have to try different combinations of actors
+    remaining_nodes.to_a.permutations(remaining_nodes.size).map do |next_nodes|
+      ready_actors.permutations(remaining_nodes.size).map do |actor_group|
+        new_actors = actor_group.zip(next_nodes).map do |actor, next_node|
+          distance_to_next_node = map[actor.moving_to][next_node]
+          actor.moving_to = next_node
+          actor.distance = distance_to_next_node
+          actor
+        end
 
-    max_release(new_time_left, new_release, next_node, map, remaining_nodes - [next_node])
-  end.max
+        new_remaining_nodes = remaining_nodes - next_nodes
+        next_actors = moving_actors + new_actors
+
+        max_release_at_minute(minutes_left, next_actors, current_release, map, new_remaining_nodes)
+      end.max
+    end.max
+  else
+    remaining_nodes.to_a.permutations(ready_actors.size).map do |next_nodes|
+      new_actors = ready_actors.zip(next_nodes).map do |actor, next_node|
+        distance_to_next_node = map[actor.moving_to][next_node]
+        actor.moving_to = next_node
+        actor.distance = distance_to_next_node
+        actor
+      end
+
+      new_remaining_nodes = remaining_nodes - next_nodes
+      next_actors = moving_actors + new_actors
+
+      max_release_at_minute(minutes_left, next_actors, current_release, map, new_remaining_nodes)
+    end.max
+  end
 end
 
 AOC.part1 do
   initial_distances.map do |first_node, distance|
-    time_left = 30 - distance - 1
+    actors = [Actor.new(first_node, distance)]
+    max_release_at_minute(30, actors, 0, map, key_set - [first_node])
+  end.max
+end
 
-    release = time_left * VALVES[first_node].flow_rate
-
-    max_release(time_left, release, first_node, map, key_set - [first_node])
+# TODO: This is quite slow, figure out some optimizations
+AOC.part2 do
+  initial_distances.to_a.combinations(2).map do |initial_nodes|
+    actors = initial_nodes.map { |first_node, distance| Actor.new(first_node, distance) }
+    max_release_at_minute(26, actors, 0, map, key_set - actors.map(&.moving_to))
   end.max
 end
