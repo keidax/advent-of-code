a764b9fe0ceee9dbdab526e8d3c47d3b6df0d159
Gabriel Holodak <gthepiper@gmail.com>
2023-12-01 01:13:01 -0500

    update crystal helper for 2023

---
+++ 2023/aoc.cr
@@ -0,0 +1,77 @@
+require "http/request"
+require "http/client"
+
+module AOC
+  YEAR       = 2023
+  INPUT_PATH = "input.txt"
+
+  @@day : Int32 = 0
+
+  def self.day!(day : Int32)
+    @@day = day
+  end
+
+  @@input_loaded = false
+  @@input : String = ""
+
+  def self.each_line
+    load_input!
+
+    @@input.each_line do |line|
+      yield line
+    end
+  end
+
+  def self.lines
+    load_input!
+
+    @@input.lines
+  end
+
+  def self.input
+    load_input!
+
+    @@input
+  end
+
+  def self.part1
+    log("part 1 : #{yield}")
+  end
+
+  def self.part2
+    log("part 2 : #{yield}")
+  end
+
+  private def self.load_input!
+    unless @@input_loaded
+      @@input = load_input(@@day)
+    end
+  end
+
+  private def self.load_input(day)
+    input_path =
+      if File.exists?(INPUT_PATH)
+        File.read(INPUT_PATH)
+      else
+        download_file(day)
+      end
+  end
+
+  private def self.download_file(day)
+    log ">> downloading input for the first time"
+
+    client = HTTP::Client.new("adventofcode.com", tls: true)
+    get_input = HTTP::Request.new(method: "GET", resource: "/#{YEAR}/day/#{day}/input")
+    get_input.cookies["session"] = ENV["AOC_SESSION"]
+
+    response = client.exec get_input
+    input = response.body
+
+    File.write(INPUT_PATH, input)
+    input
+  end
+
+  private def self.log(message)
+    puts "❄️  #{message}"
+  end
+end
