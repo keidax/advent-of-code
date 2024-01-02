78e5dc457d43b9056e860ad741a7528e016c2bef
Gabriel Holodak <gthepiper@gmail.com>
2024-01-02 13:26:29 -0500

    Download inputs through Godot

--- godot-animations/Input.gd
+++ godot-animations/Input.gd
@@ -11,12 +11,21 @@ signal available(data)
 func _ready():
 	if !FileAccess.file_exists(file_path):
 		download_data()
+		await $HTTPRequest.request_completed
 	
 	var data = FileAccess.get_file_as_string(file_path)
 	var error = FileAccess.get_open_error()
 	assert(error == OK, error_string(error))
 	
-	available.emit(data)
+	available.emit(data.strip_edges())
 
 func download_data():
-	pass
+	var session_token = OS.get_environment("AOC_SESSION")
+	assert(!session_token.is_empty(), "missing AOC_SESSION env var")
+	
+	var url = "https://adventofcode.com/%d/day/%d/input" % [year, day]
+	var headers = ["Cookie: session=%s" % session_token]
+	
+	$HTTPRequest.download_file = file_path
+	var error = $HTTPRequest.request(url, headers)
+	assert(error == OK, error_string(error))
