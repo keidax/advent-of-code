335e79e11ad6b4e9b91e725e7e413f8673493810
Gabriel Holodak <gthepiper@gmail.com>
2024-12-11 12:37:07 -0500

    optimize day 7

--- 2024/07.rb
+++ 2024/07.rb
@@ -4,56 +4,40 @@ input = AOC.day(7)
 
 equations = input.lines(chomp: true).map do |line|
   value, numbers = line.split(": ")
-  [value.to_i, numbers.split(" ").map(&:to_i).reverse]
+  [value.to_i, numbers.split(" ").map(&:to_i)]
 end
 
-# optimized to avoid allocations by mutating the numbers array instead of
-# copying it
-def can_calibrate?(value, numbers, ops)
-  a = numbers.pop
-  b = numbers.pop
+def solvable?(value, numbers, concat: false)
+  last_num = numbers.last
 
-  ops.each do |op|
-    c = a.send(op, b)
+  if numbers.size == 1
+    return last_num == value
+  end
 
-    numbers.push c
+  if value % last_num == 0
+    return true if solvable?(value / last_num, numbers[..-2], concat:)
+  end
 
-    if c > value
-      next
-    end
+  if value > last_num
+    return true if solvable?(value - last_num, numbers[..-2], concat:)
+  end
 
-    if numbers.size == 1
-      if value == numbers.first
-        return true
-      end
-    elsif can_calibrate?(value, numbers, ops)
-      return true
-    end
-  ensure
-    numbers.pop # c
+  if concat && value.to_s.end_with?(last_num.to_s)
+    places = Math.log10(last_num).to_i + 1
+    return true if solvable?(value / (10**places), numbers[..-2], concat:)
   end
 
   false
-ensure
-  numbers.push b
-  numbers.push a
-end
-
-class Integer
-  def concat(other)
-    places = Math.log10(other).to_i + 1
-    self * (10**places) + other
-  end
 end
 
 AOC.part1 do
   equations
-    .select { can_calibrate?(_1[0], _1[1], [:+, :*]) }
+    .select { solvable?(*_1, concat: false) }
     .sum { _1[0] }
 end
 
 AOC.part2 do
   equations
-    .select { can_calibrate?(_1[0], _1[1], [:+, :*, :concat]) }
+    .select { solvable?(*_1, concat: true) }
     .sum { _1[0] }
 end
