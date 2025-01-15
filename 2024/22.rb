547aca043d4c59a2c6a14dd788fede4f005eb358
Gabriel Holodak <gthepiper@gmail.com>
2025-01-15 13:56:00 -0500

    optimize day 22 a bit more

--- 2024/22.rb
+++ 2024/22.rb
@@ -1,81 +1,53 @@
 require_relative "aoc"
 
-class SequenceTree
-  def initialize(height)
-    @height = height
-    @children = Array.new(19, nil)
-  end
-
-  def new_child
-    if @height > 1
-      SequenceTree.new(@height - 1)
-    else
-      SequenceCounter.new
-    end
-  end
-
-  def store(seq, id, price)
-    change = seq[-@height]
-    next_level = @children[change] ||= new_child
-    next_level.store(seq, id, price)
-  end
-
-  def max
-    @children.map { _1&.max || 0 }.max
-  end
-end
-
-class SequenceCounter
-  def initialize
-    @sum_cache = {}
-  end
-
-  def store(_seq, id, price)
-    unless @sum_cache.key?(id)
-      @sum_cache[id] = price
-    end
-  end
-
-  def max
-    @sum_cache.values.sum
-  end
-end
-
-SEQUENCE_TREE = SequenceTree.new(4)
-
 class Buyer
-  attr_reader :secrets, :prices, :price_changes
+  LOW_15_BITS = 2**15 - 1
+
+  # SEQUENCE_TREE is a map of sequences of price changes to a map of the price
+  # each buyer will sell at when that sequence is first seen. The price change
+  # sequence is represented as a 20-bit integer instead of a 4-integer array.
+  # This optimizes memory allocations and hash lookup speed. Buyers are mapped
+  # by their initial secret value, which we assume is unique.
+  SEQUENCE_TREE = Hash.new { |h, k| h[k] = {} }
+
+  attr_reader :last_secret
 
   def initialize(secret)
     @initial_secret = secret
 
-    @secrets = [@initial_secret]
-
     prev_secret = @initial_secret
     prev_price = @initial_secret % 10
-    price_changes = []
+    price_changes = 0
 
-    2000.times do
+    2000.times do |i|
       new_secret = next_secret(prev_secret)
       new_price = new_secret % 10
 
-      @secrets << new_secret
-      price_changes << (new_price - prev_price)
+      # make sure price_change is in the range 0..18
+      price_change = (new_price - prev_price) + 9
+      # price_change requires 5 bits
+      price_changes = (price_changes << 5) | price_change
 
-      if price_changes.size == 4
-        SEQUENCE_TREE.store(price_changes, @initial_secret, new_price)
-        price_changes.shift
+      if i >= 3
+        SEQUENCE_TREE[price_changes][@initial_secret] ||= new_price
+        price_changes &= LOW_15_BITS
       end
 
       prev_secret = new_secret
       prev_price = new_price
     end
+
+    @last_secret = prev_secret
   end
 
   def next_secret(secret)
     secret = (secret ^ (secret << 6)) % 16777216
     secret = (secret ^ (secret >> 5)) % 16777216
     (secret ^ (secret << 11)) % 16777216
+  end
+
+  def self.max_bananas
+    SEQUENCE_TREE.values.map { _1.values.sum }.max
   end
 end
 
@@ -84,9 +56,9 @@ initial_secrets = input.lines(chomp: true).map(&:to_i)
 buyers = initial_secrets.map { Buyer.new(_1) }
 
 AOC.part1 do
-  buyers.sum { _1.secrets[2000] }
+  buyers.sum(&:last_secret)
 end
 
 AOC.part2 do
-  SEQUENCE_TREE.max
+  Buyer.max_bananas
 end
